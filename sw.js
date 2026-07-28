1a6262897c58f194990d28b00710a1fc7ff10b6e
madhustylizz <madhustylizz@gmail.com>
2026-07-28 22:45:42 +0530

    Cache Mechanism

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'routeledger-v1';
+// Change 'v1' to 'v2', 'v3', etc., whenever you deploy an update
+const CACHE_NAME = 'routeledger-v2'; 
+
 const ASSETS = [
   '/',
   '/index.html',
@@ -10,9 +12,27 @@ const ASSETS = [
   '/js/config.js'
 ];
 
+// Install Event
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Force new service worker to activate immediately
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activate Event: Delete old caches automatically
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('Deleting old cache:', key);
+            return caches.delete(key); // Clear old cache version
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
